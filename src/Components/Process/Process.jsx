7dcacbd43d7a2019aa685f301d615ce8ac61eafa
varunjasuja007 <jasujavarun123@gmail.com>
2026-07-28 15:13:29 +0530

    .

--- src/Components/Process/Process.jsx
+++ src/Components/Process/Process.jsx
@@ -8,13 +8,13 @@ const Process = () => {
 
     const rendersteps = steps.map(item => {
         return (
-            <div className="flex-1">
-                <span className=" flex justify-center items-center w-18 h-18  rounded-full text-8xl bg-zinc-800 text-white outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed ">{item.number}</span>
+            <div className={`flex-1 basis-[300px] ${item.id % 2 === 0 ? 'md:-mt-100' : ''}`}>
+                <span className=" flex justify-center items-center w-18 h-18 mx-auto rounded-full text-8xl bg-zinc-800 text-white outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed ">{item.number}</span>
 
-                <div className="flex items-center">
-                    <span className="flex justify-center items-center text-3xl bg-gradient-to-b from-orange-400 to-orange-500 text-white  h-10 rounded-full">{item.icon}</span>
+                <div className="flex items-center gap-x-5 mt-10 ">
+                    <span className="flex justify-center items-center text-3xl bg-gradient-to-b from-orange-400 to-orange-500 text-white w-12 h-12 rounded-full">{item.icon}</span>
 
-                    <div>
+                    <div className="flex-1">
                         <h4 className="text-zinc-800 text-2xl font-bold">{item.title}</h4>
                         <p className="text-zinc-600 mt-2">{item.para}</p>
                     </div>
@@ -24,11 +24,11 @@ const Process = () => {
     })
     return (
         <section>
-            <div className="mx-w-[1400px] mx-auto px-10 py-20">
+            <div className="max-w-[1400px] mx-auto px-10 py-20">
                 <div className=" w-fit mr-auto ">
-                    <Heading highlight="Our" heading="Procees" />
+                    <Heading highlight="Our" heading="Process" />
                 </div>
-                <div className="flex mt-20 items-center justify-center ">
+                <div className="flex flex-wrap gap-y-17 md:mt-20 mt-10 items-center justify-center md:pt-40 ">
                     {rendersteps}
                 </div>
 
@@ -47,8 +47,8 @@ const steps = [
     {
         id: 1,
         number: <TbCircleNumber1Filled />,
-        title: 'sourcing',
-        para: 'Our experienced designers and skilled craftsmen create stylish, durable, and comfortable clothing with attention to every detail.',
+        title: 'Sourcing',
+        para: 'Premium fabrics sourced from trusted suppliers.',
         icon: <PiPlant />
     },
 
@@ -56,21 +56,21 @@ const steps = [
         id: 2,
         number: <TbCircleNumber2Filled />,
         title: 'Manufacturing',
-        para: 'Our experienced designers and skilled craftsmen create stylish, durable, and comfortable clothing with attention to every detail.',
+        para: 'Crafted with precision by skilled professionals.',
         icon: <PiFactory />
     },
     {
         id: 3,
         number: <TbCircleNumber3Filled />,
         title: 'Quality Inspection',
-        para: 'Every product undergoes multiple quality checks to ensure perfect stitching, fit, comfort, and finishing before delivery.',
+        para: 'Every product is carefully quality checked.',
         icon: <FaAward />
     },
     {
-        id: 3,
+        id: 4,
         number: <TbCircleNumber4Filled />,
         title: 'Fast Delivery',
-        para: 'Your order is carefully packed and delivered quickly to your doorstep with reliable tracking and secure packaging.',
+        para: 'Secure packaging with fast doorstep delivery.',
         icon: <TbTruck />
     },
 
