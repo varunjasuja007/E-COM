e993d001a3b01e0b19b008b2b01ab1c38787bc58
varunjasuja007 <jasujavarun123@gmail.com>
2026-07-30 20:39:03 +0530

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Footer = () => {
     return (
@@ -44,20 +45,26 @@ const Footer = () => {
                         <a href="#" className="text-zinc-800 hover:text-orange-500"> Feedback</a>
                     </li>
                     <li className="mt-6">
-                        <a href="#" className="text-zinc-800 hover:text-orange-500"> Contect Us</a>
+                        <a href="#" className="text-zinc-800 hover:text-orange-500"> Contact Us</a>
                     </li>
 
                 </ul>
 
                 <div>
-                    <h5 className="text-zonc-800 text-2xl font-bold" >Stay Connected</h5>
+                    <h5 className="text-zinc-800 text-2xl font-bold" >Stay Connected</h5>
 
                     <p className="mt-6 text-zinc-600">
                         Questions or Feedbacks <br />
                         We'd love to hear from you.
                     </p>
-                </div>
 
+                    <div className="flex bg-white p-1 rounded-lg mt-6 ">
+                        <input type="email" name="email" id="email" autoCorrect='off' placeholder="Email Address" className="h-[5vh] pl-4 flex-1 focus:outline-none" />
+                        <button className="bg-gradient-to-b from-orange-400 to-orange-500 p-2 rounded-lg text-white text-2xl hover:to-orange-600 cursor-pointer">
+                            <IoIosArrowForward />
+                        </button>
+                    </div>
+                </div>
             </div>
 
         </footer>
